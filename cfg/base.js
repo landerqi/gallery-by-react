9a904b52ab4843850268051026353888a9b19a50
谢奇 <landerqi@qq.com>
2016-12-02 14:39:38 +0800

    增加json-loader

--- cfg/base.js
+++ cfg/base.js
@@ -60,6 +60,10 @@ module.exports = {
         loader: 'style-loader!css-loader!stylus-loader'
       },
       {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192'
       }
