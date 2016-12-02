dd6a7783641ac73e1fc2f9482e8575d5f243357b
谢奇 <landerqi@qq.com>
2016-12-02 14:44:09 +0800

    init project structrue

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,31 @@
 require('normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let imageDatas = require('../data/imagedatas.json');
+
+function genImageURL(imageDataArr) {
+    for (var i = 0, j = imageDataArr.length; i < j; i++) {
+        var singleImageData = imageDataArr[i];
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+        imageDataArr[i] = singleImageData;
+    }
+
+    return imageDataArr;
+}
+
+imageDatas = genImageURL(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <section className = "stage">
+            <section className = "img-sec">
+            </section>
+            <nav className = "controllor-nav">
+            </nav>
+        </section>
     );
   }
 }
