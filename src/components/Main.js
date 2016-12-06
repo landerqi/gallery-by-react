4593a1983a9ddacfb2673e3d3f0be5ef2a09970d
谢奇 <landerqi@qq.com>
2016-12-06 17:23:33 +0800

    代码提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let imageDatas = require('../data/imagedatas.json');
+let imageDatas = require('json!../data/imagedatas.json');
 
 function genImageURL(imageDataArr) {
     for (var i = 0, j = imageDataArr.length; i < j; i++) {
@@ -17,17 +17,44 @@ function genImageURL(imageDataArr) {
 
 imageDatas = genImageURL(imageDatas);
 
+
+class ImgFigure extends React.Component {
+    render() {
+        return (
+            <figure>
+                <img className = "img-little"
+                     src = {this.props.data.imageURL}
+                     alt = {this.props.data.title}
+                />
+                <figcaption>
+                    <h2>{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        )
+    }
+}
+
 class AppComponent extends React.Component {
-  render() {
-    return (
-        <section className = "stage">
-            <section className = "img-sec">
+    render() {
+        var controllerUnits = [],
+            imgFigures = [];
+
+        imageDatas.forEach(function(value, index){
+            imgFigures.push(<ImgFigure data={value} key={'imgFigures' + index} />);
+
+        });
+
+        return (
+            <section className = "stage">
+                <section className = "img-sec">
+                    {imgFigures}
+                </section>
+                <nav className = "controllor-nav">
+                    {controllerUnits}
+                </nav>
             </section>
-            <nav className = "controllor-nav">
-            </nav>
-        </section>
-    );
-  }
+        );
+    }
 }
 
 AppComponent.defaultProps = {
