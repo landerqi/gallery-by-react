a6a4c63da71a1a510bbabc689ee79a5d3e5aa6c5
Landerqi <landerqi@gmail.com>
2016-12-08 18:22:24 +0800

    '添加图片区域坐标'

--- src/components/Main.js
+++ src/components/Main.js
@@ -49,25 +49,29 @@ class ImgFigure extends React.Component {
 }
 
 class AppComponent extends React.Component {
+
 	constructor(props) {
 		super(props);
-	}
 
-	Constant: {
-		centerPos: {
-			left: 0,
-			rigth: 0
-		},
-		hPosRange: {   //水平方向取值范围
-			leftSecX: [0, 0],
-			rightSecX: [0,0],
-			y: [0, 0]
-		},
-		vPosRange: {  //垂直方向取值范围
-			x: [0, 0],
-			topY: [0, 0]
+		this.rearrange = this.rearrange.bind(this);
+
+		this.Constant = {
+			centerPos: {
+				left: 0,
+				rigth: 0
+			},
+			hPosRange: {   //水平方向取值范围
+				leftSecX: [0, 0],
+				rightSecX: [0,0],
+				y: [0, 0]
+			},
+			vPosRange: {  //垂直方向取值范围
+				x: [0, 0],
+				topY: [0, 0]
+			}
 		}
 	}
+
 
 	//组件加载后，为每张图片计算其位置的范围
 	componentDidMount() {
@@ -78,6 +82,42 @@ class AppComponent extends React.Component {
 			halfStageW = Math.ceil(stageW / 2),
 			halfStageH = Math.ceil(stageH / 2);
 
+		//拿到一个imageFigure的大小
+		let ImgFigureDOM = ReactDOM.findDOMNode(this.refs.ImgFigure0),
+			imgW = ImgFigureDOM.scrollWidth,
+			imgH = ImgFigureDOM.scrollHeight,
+			halfImgW = Math.ceil(imgW / 2),
+			halfImgH = Math.ceil(imgH / 2);
+
+		//计算中心图片的位置点
+		this.Constant.centerPos = {
+			left: halfStageW - halfImgW,
+			top: halfStageH - halfImgH
+		}
+
+		//计算左侧，右侧区域图片排布位置的取值范围
+		this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+		this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+		this.Constant.hPosRange.rightSecX[0] = halfStageW - halfImgW;
+		this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+		this.Constant.hPosRange.y[0] = -halfImgH;
+		this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+		//计算上侧区域图片排布位置的取值范围
+		this.Constant.vPosRange.topY[0] = -halfImgH;
+		this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+		this.Constant.vPosRange.x[0] = halfImgW - imgW;
+		this.Constant.vPosRange.x[1] = halfImgW;
+
+		this.rearrange(0);
+	}
+
+	/*
+	 * 重新布局所有图片
+	 * @pram Number centerIndex 指定居中排布哪个图片
+	 * */
+	rearrange(centerIndex) {
+
 	}
 
     render() {
@@ -85,7 +125,7 @@ class AppComponent extends React.Component {
             imgFigures = [];
 
         imageDatas.forEach(function(value, index){
-            imgFigures.push(<ImgFigure data={value} key={'imgFigures' + index} />);
+            imgFigures.push(<ImgFigure data={value} key={'imgFigures' + index} ref={'ImgFigure' + index} />);
 
         });
 
